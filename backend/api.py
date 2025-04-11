d2458b7344b6006afe5faf89d4ec4327763eab07
aditissigh24 <aditissigh24@gmail.com>
2025-04-11 18:34:41 +0530

    fix bug

--- backend/api.py
+++ backend/api.py
@@ -10,7 +10,7 @@ import chromadb
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://askpdf-phi.vercel.app"],
+    allow_origins=["http://localhost:5173","https://askpdf-phi.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
